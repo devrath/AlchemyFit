30ad7ed5dc183e5ec8e1e6cdf139314d501fa64e
Devrath <devrath.dev595@gmail.com>
2025-09-20 12:44:55 +0530

    initial commit

---
+++ code/composeApp/src/commonTest/kotlin/org/istudio/vitalchemy/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.istudio.vitalchemy
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
